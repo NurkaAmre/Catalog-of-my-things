662a5376efd097ec4af08f75c61a6c76422692b0
Nurgul Kereikhan <nurkerey30@gmail.com>
2023-01-15 16:21:33 +0000

    Create main.rb & app.rb

---
+++ app.rb
@@ -0,0 +1,17 @@
+class App
+  def initialize
+    @books = []
+    @music_albums = []
+    @games = []
+    @movies = []
+    
+    @genres = []
+    @authors = []
+    @labels = []
+    @sources = []
+  end
+
+  def run
+    puts 'Please select an option'
+  end
+end
