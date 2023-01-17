d736ff7a2a522d6a9fadd55b0365f9826037a4c0
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2023-01-17 19:02:57 +0600

    Create read, save Data for music & genre

--- f_data_json/read_data.rb
+++ f_data_json/read_data.rb
@@ -20,4 +20,24 @@ module DataReader
       File.write('./f_data_json/labels.json', [])
     end
   end
+
+  def load_music_album
+    if File.exist?('./f_data_json/music_album.json')
+      listmusic = File.open('./f_data_json/music_album.json')
+      info = listmusic.read
+      JSON.parse(info)
+    else
+      File.write('./f_data_json/music_album.json', [])
+    end
+  end
+
+  def load_genres
+    if File.exist?('./f_data_json/genre.json')
+      listgenres = File.open('./f_data_json/genre.json')
+      info = listgenres.read
+      JSON.parse(info)
+    else
+      File.write('./f_data_json/genre.json', [])
+    end
+  end
 end
