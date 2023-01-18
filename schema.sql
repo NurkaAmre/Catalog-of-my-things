86b50687b870d8eaa87bab4f3c7bd8a42b762479
Usirusen Ikon <lexicon048@gmail.com>
2023-01-18 16:17:37 +0100

    Create table for games and authors

--- schema.sql
+++ schema.sql
@@ -1,9 +1,13 @@
 CREATE TABLE items (
   id INT PRIMARY KEY,
   genre_id INT,
+  label_id INT,
+  author_id INT,
   publish_date DATE,
   archived BOOLEAN,
   FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
 );
 
 CREATE TABLE music_albums (
@@ -29,8 +33,24 @@ CREATE TABLE labels (
   color VARCHAR(255)
 );
 
+CREATE TABLE games (
+  id INT PRIMARY KEY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY (id) REFERENCES items(id)
+);
+
+CREATE TABLE authors (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(225),
+  last_name VARCHAR(225)
+);
+
 Select * From music_albums;
 Select * From genres;
 
 Select * From books;
 Select * From labels;
+
+Select * From games;
+Select * From authors;
