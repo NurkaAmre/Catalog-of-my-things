aab540926bb2fd2fc3b61b7788a778a4b79b7ee4
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2023-01-19 17:12:09 +0600

    Rspec for genre

--- f_spec/genre_spec.rb
+++ f_spec/genre_spec.rb
@@ -2,24 +2,14 @@ require './f_classes/item'
 require './f_classes/things/f_associations/genre'
 require './app'
 
-describe Label do
-  context 'Given an instance of a book' do
+describe Genre do
+  context 'Given an instance of a genre' do
     before :each do
-      @labels = Label.new('gift', 'red')
+      @genres = Genre.new('Country')
     end
 
     it 'returns a new label' do
-      expect(@labels).to be_instance_of Label
-    end
-
-    it 'check correct title' do
-      @labels.title = 'gift'
-      expect(@labels.title).to eql 'gift'
-    end
-
-    it 'check correct title' do
-      @labels.color = 'red'
-      expect(@labels.color).to eql 'red'
+      expect(@genres).to be_instance_of Genre
     end
   end
 end
