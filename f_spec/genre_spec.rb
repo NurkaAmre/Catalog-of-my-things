845d9490aa64639d8d67d0ca38fc4a6a2a71cc13
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2023-01-19 16:07:58 +0600

    Rspec test music_spec

--- f_spec/genre_spec.rb
+++ f_spec/genre_spec.rb
@@ -1,1 +1,25 @@
-puts 'genre.test'
+require './f_classes/item'
+require './f_classes/things/f_associations/genre'
+require './app'
+
+describe Label do
+  context 'Given an instance of a book' do
+    before :each do
+      @labels = Label.new('gift', 'red')
+    end
+
+    it 'returns a new label' do
+      expect(@labels).to be_instance_of Label
+    end
+
+    it 'check correct title' do
+      @labels.title = 'gift'
+      expect(@labels.title).to eql 'gift'
+    end
+
+    it 'check correct title' do
+      @labels.color = 'red'
+      expect(@labels.color).to eql 'red'
+    end
+  end
+end
